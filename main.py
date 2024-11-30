186a3572ce463475002c1bc691fea45dc8a8517b
Nitskor <koruthunitin@gmail.com>
2024-11-30 14:43:50 +0530

    Effectively clear previous conversations using checkbox

--- main.py
+++ main.py
@@ -13,29 +13,31 @@ if 'previous_questions' not in st.session_state:
 if 'previous_answers' not in st.session_state:
     st.session_state.previous_answers = []
 
-
+if st.session_state.previous_questions:
+    with st.expander("Previous Questions and Answers"):
+        for i in range(len(st.session_state.previous_questions)):
+            st.write(f"**Question {i+1}**: {st.session_state.previous_questions[i]}")
+            st.code(st.session_state.previous_answers[i], language='markdown')
+            st.write("\n")
 
 with st.form("Code Helper"):
     # Your form fields here
-    question = st.text_input("How can I help you?")
-    clear_previous = st.checkbox("Clear Previous Questions and Answers")    
+    question = st.text_input("How can I help you?")      
     submit_button = st.form_submit_button("Submit")
+    
+clear_button = st.checkbox("Clear Previous Questions and Answers")
 
 #st.title("Code Helper")
 #question = st.text_input("How can I help you?")
 #submit_button = st.button(key="Submit")
 
-if submit_button:
-  if clear_previous:
-     st.session_state.previous_questions = []
-     st.session_state.previous_answers = []
+if submit_button:  
   result = llm.invoke(question)
   answer = result.content  
   st.session_state.previous_questions.append(question)
   st.session_state.previous_answers.append(answer)   
   st.code(answer,language='markdown')
 
-for i in range(len(st.session_state.previous_questions)):
-   st.write(f"**Question {i+1}**: {st.session_state.previous_questions[i]}")
-   st.code(st.session_state.previous_answers[i], language='markdown')
-   st.write("\n")
+if clear_button:
+    st.session_state.previous_questions = []
+    st.session_state.previous_answers = []
